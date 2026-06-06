cc7dd8ece262e40e224c3a533692e889841e3488
Jhonatan Sousa Meurer <jhonatansousameurer@gmail.com>
2026-06-06 16:28:54 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Window window = new Window(new AppShell());
+
+            window.Width = AutomationIdProperty.DefaultValue switch
+            {
+                "iOS" => 375,
+                "Android" => 400,
+                _ => 1280
+            };
+            window.Height = AutomationIdProperty.DefaultValue switch
+            {
+                "iOS" => 667,
+                "Android" => 700,
+                _ => 720
+
+            };
+
+            return window;
+        }
+    }
+}
